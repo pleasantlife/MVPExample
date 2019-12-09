7bd845b33e84d0640e81f10e309df18e8d71b778
pleasantlife <pleasantlife@gmail.com>
2019-12-09 23:47:48 +0900

    191209 retrofit added.

--- app/src/main/java/com/gandan/mvpexample/MainRepository.java
+++ app/src/main/java/com/gandan/mvpexample/MainRepository.java
@@ -1,21 +1,27 @@
 package com.gandan.mvpexample;
 
-import android.util.Log;
-
-import java.util.Arrays;
-import java.util.List;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainRepository implements Repository {
 
-
+    private String code = "";
     @Override
     public String load() {
-        List<String> list = Arrays.asList("티몬", "쿠팡", "마켓컬리", "아마존", "옥션", "지마켓", "이마트");
-        int random = ((int) (Math.random() * 10) + 1);
-        if(random > 6){
-            random = 6;
-        }
-        Log.e("random", random+"");
-        return list.get(random);
+
+        RetrofitUtil.getRetrofitService().getTodo(1).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                code = response.code()+"";
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                code = t.getMessage()+"";
+            }
+        });
+        return code;
     }
 }
